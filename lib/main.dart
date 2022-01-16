01f392f873459141bb5cc530743301ea6ffe225d
jesuskaz <jesuskazkid@gmail.com>
2022-01-16 18:26:24 +0200

    The init project

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:oasis/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+     return MaterialApp(
+       debugShowCheckedModeBanner: false,
+       theme: ThemeData(
+            appBarTheme: AppBarTheme(
+              backgroundColor: Color(0xff075e54)
+            ),
+           // primarySwatch: Colors.green,
+           // primaryColor: Colors.green,
+           primaryColorLight: Colors.green,
+       ),
+       home: HomeScreen(),
+
+     );
+  
+  }
+}
+
